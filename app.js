e0d586c1bd69a9f49bd8d5aa8e8bfb09612b95ed
IngLuisNavas <ingluisnavas@gmail.com>
2021-11-22 13:19:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+const { request, response } = require("express");
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 
+app.get('/api/personas', (request, response) => {
+    response.status(200).json(personas);
+})
+
+app.post('api/personas', (request, response) => {
+    let persona = request.body;
+    console.log(persona);
+    personas.push(persona);
+    response.status(201).json(personas);
+
+
+})
 
 app.get('/', (request, response) => {
 
@@ -12,3 +25,10 @@ app.get('/', (request, response) => {
 app.listen(1600, () => {
     console.log("Conectado al servidor");
 });
+
+let personas = [{ nombre: "Laura Marcela", apellido: "Torres Diaz", edad: "21" },
+    { nombre: "Jesus Emel", apellido: "Gomez Gomez", edad: "31" },
+    { nombre: "Luis Eduardo", apellido: "Jerez Ruiz", edad: "33" },
+
+
+];
